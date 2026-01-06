a75e26a267b4f6efc7ed29663055b1a06fe714eb
amt-kumar-kushwaha <amitkumarkushwaha.com.np@gmail.com>
2026-01-06 19:57:38 +0545

    basic

---
+++ app.js
@@ -0,0 +1,6 @@
+// const a = 22;
+export const arr = [10, 20, 30, 40];
+
+const user = "Amit";
+
+export default user;
---
+++ script.js
@@ -0,0 +1,14 @@
+import username from "./app.js";
+import { arr } from "./app.js";
+
+// var h1 = document.createElement("h1");
+// console.log(h1);
+
+// h1.innerHTML = "Hello From JS";
+
+// document.body.appendChild(h1);
+
+// console.log(a);
+// // console.log(arr);
+// console.log(username);
+console.log(arr);
